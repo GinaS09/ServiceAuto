7b5e17368c85ab590fac963af512f9f2e9f59e30
Gina Marina <gina.seres58@gmail.com>
2020-11-15 21:10:47 +0200

    Adauga README & iServiceAuto interfata

---
+++ ServiceAuto/src/iServiceAuto.java
@@ -0,0 +1,10 @@
+public interface iServiceAuto {
+    public void setCar(String car);
+    public void setRepare(String repare);
+
+    public String getCar();
+    public String getRepare();
+
+    public void Pay();
+
+}
